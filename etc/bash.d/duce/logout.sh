7f049c5d99d4fd5b76ecd7d57a76e3fd69f85354
Duce Tate <ducet8@outlook.com>
2023-02-09 14:59:47 -0600

    cleanup

--- etc/bash.d/duce/logout.sh
+++ etc/bash.d/duce/logout.sh
@@ -2,7 +2,6 @@
 # 2023.02.09 - ducet8@outlook.com
 
 function bash_logout() {
-
     if [[ "${BD_OS,,}" =~ 'alpine' ]] || [[ "${BD_OS,,}" == 'darwin' ]]; then 
         local bash_logins=($(ps aux | grep ${USER} | grep -c [b]ash))
     else
@@ -16,7 +15,6 @@ function bash_logout() {
     fi
 
     bash_logout_message="$(date) logout ${USER}@${HOSTNAME} (bash_logins=${bash_logins})"
-
     bd_ansi reset
     if [ ${bash_logins} -lt 2 ]; then
         sleep_timer=5
@@ -52,7 +50,7 @@ function bash_logout() {
     fi
 }
 
-# Trap EXIT; ensure bash_logout gets called, regardless
+# Trap EXIT
 if  [ "$(type -t bash_logout)" == "function" ]; then
     if [ ${#TMUX_PANE} -eq 0 ]; then
         trap "bash_logout" EXIT
