77f500355d6d030868011172f68967aabacf7942
Duce Tate <ducet8@outlook.com>
2023-03-14 13:22:30 -0500

    dexec initial testing fixes

--- etc/bash.d/duce/dexec.sh
+++ etc/bash.d/duce/dexec.sh
@@ -8,7 +8,7 @@ function dexec() {
     [ -z ${container_shell} ] && container_shell=$(sudo docker exec nginx sh -c "which sh")
     [ -z ${container_shell} ] && bd_ansi fg_red && echo "ERROR: Could not determine the shell for container: ${1}" && bd_ansi reset && return 1
 
-    sudo docker cp $BD_HOME/dotfiles/container_profile "${1}":/tmp/duce
+    sudo docker cp -q $BD_HOME/dotfiles/container_profile "${1}":/tmp/duce
     sudo docker exec -it "${1}" "${container_shell}"
-    sudo docker exec "${1}" "${container_shell}" -c "[[ -f /tmp/duce ]] && rm duce"
+    sudo docker exec "${1}" "${container_shell}" -c "[[ -f /tmp/duce ]] && rm /tmp/duce"
 }
