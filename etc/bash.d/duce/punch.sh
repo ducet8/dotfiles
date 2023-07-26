c496fdd9da74f346c3b7ce09b0e67f140f54459d
Duce Tate <dtate@CSB-45487-BHM>
2023-07-26 11:51:58 -0500

    Adding error checks and options to punch

--- etc/bash.d/duce/punch.sh
+++ etc/bash.d/duce/punch.sh
@@ -1,13 +1,25 @@
-# 2023.07.21 - ducet8@outlook.com
+# vim: ft=sh
+# 2023.07.26 - ducet8@outlook.com
 
-function punch() {
+punch() {
     if [ ! "${1}" ]; then
 	echo "punch must be run with an argument to be used as the note"
 	return 1
     fi
 
-    # TODO: Add 'doh' parameter to remove last entry
-    if [ "${1}" == 'in' ]; then
+    if [[ "$(tail -1 ${HOME}/.timecard | awk '{print $2}')" != "$(date '+%A')" ]] && [[ ! -z "$(tail -1 ${HOME}/.timecard)" ]]; then
+	if [ "$(date '+%A')" == 'Monday' ]; then
+	    echo "####" >>${HOME}/.timecard
+	fi
+	echo >>${HOME}/.timecard
+    fi
+
+    if [ "${1}" == 'doh' ]; then
+	sed -i '$d' ${HOME}/.timecard
+	if [[ -z "$(tail -1 ${HOME}/.timecard)" ]]; then
+	    sed -i '$d' ${HOME}/.timecard
+	fi
+    elif [ "${1}" == 'in' ]; then
         echo "[$(date '+%Y-%m-%d %A %H:%M:%S')] - punch in" >>${HOME}/.timecard
     elif [ "${1}" == 'out' ]; then
         echo "[$(date '+%Y-%m-%d %A %H:%M:%S')] - punch out" >>${HOME}/.timecard
