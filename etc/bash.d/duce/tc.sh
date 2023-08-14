c24ff21f82b7d41d5b4f002ce7b0a57812c7e01b
Duce Tate <dtate@ENGR-45429-MAC>
2023-08-14 08:14:44 -0500

    bug fixing and versioning tc

--- etc/bash.d/duce/tc.sh
+++ etc/bash.d/duce/tc.sh
@@ -2,7 +2,10 @@
 # 2023.08.11 - ducet8@outlook.com
 
 tc() {
-	local usage="$(bd_ansi fg_blue1)tc$(bd_ansi reset) 1.0.0\nUtility for interacting with timecard\n\n$(bd_ansi fg_yellow3)USAGE:\n$(bd_ansi reset)\ttc <option>\n$(bd_ansi fg_yellow3)OPTIONS:\n$(bd_ansi fg_blue1)\t-h|--help        $(bd_ansi rest)Displays this help\n$(bd_ansi fg_blue1)\t-c|--current     $(bd_ansi reset)Displays the current week's .timecard details\n$(bd_ansi fg_blue1)\t-p|--previous    $(bd_ansi reset)Displays the previous week's .timecard details\n$(bd_ansi fg_blue1)\t-e|--edit        $(bd_ansi reset)Opens a vi session for .timecard\n\n"
+    local tc_version="1.0.0-a"
+
+    local usage="$(bd_ansi fg_blue1)tc$(bd_ansi reset) ${tc_version}\nUtility for interacting with timecard\n\n$(bd_ansi fg_yellow3)USAGE:\n$(bd_ansi reset)\ttc <option>\n$(bd_ansi fg_yellow3)OPTIONS:\n$(bd_ansi fg_blue1)\t-h|--help        $(bd_ansi reset)Displays this help\n$(bd_ansi fg_blue1)\t-c|--current     $(bd_ansi reset)Displays the current week's .timecard details\n$(bd_ansi fg_blue1)\t-p|--previous    $(bd_ansi reset)Displays the previous week's .timecard details\n$(bd_ansi fg_blue1)\t-e|--edit        $(bd_ansi reset)Opens a vi session for .timecard\n\n"
+
     if [ $# -ne 1 ]; then
         printf "${usage}"
         return 1
