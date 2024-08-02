468346dc82a055a92341fce1f2745b76a9c57dd6
Duce Tate <dtate@ENGR-45429-MAC>
2024-08-02 10:31:35 -0500

    Rename the function to inclulde az

--- etc/bash.d/duce/az-aks-get-creds.sh
+++ etc/bash.d/duce/az-aks-get-creds.sh
@@ -9,20 +9,20 @@ if ! type -P az &>/dev/null; then
     return 0
 fi
 
-aks-get-creds(){
-    local aks_get_creds='1.0.0-a'
+az-aks-get-creds(){
+    local az_aks_get_creds='1.0.0-a'
 
     print_help() {
         if type bd_ansi &>/dev/null; then
             bd_ansi fg_blue1
-            printf 'aks-get-creds'
+            printf 'az-aks-get-creds'
             bd_ansi reset
-            printf "\t${aks_get_creds}\n"
+            printf "\t${az_aks_get_creds}\n"
             printf 'Outputs the required commands to get the credentials for all AKS clusters\n\n'
             bd_ansi fg_yellow3
             printf 'USAGE:\n'
             bd_ansi reset
-            printf '\taks-get-creds [OPTIONS]\n'
+            printf '\taz-aks-get-creds [OPTIONS]\n'
             bd_ansi fg_yellow3
             printf 'OPTIONS:\n'
             bd_ansi fg_blue1
@@ -35,10 +35,10 @@ aks-get-creds(){
             printf '     Displays this help message\n'
             bd_ansi reset
         else
-            printf "aks-get-creds\t${aks_get_creds}\n"
+            printf "az-aks-get-creds\t${az_aks_get_creds}\n"
             printf 'Outputs the required commands to get the credentials for all AKS clusters\n\n'
             printf 'USAGE:\n'
-            printf '\taks-get-creds [OPTIONS]\n'
+            printf '\taz-aks-get-creds [OPTIONS]\n'
             printf 'OPTIONS:\n'
             printf '\t-e|--env      The Azure environment to search [all, dev, prod] (Default: all)\n'
             printf '\t-h|--help     Displays this help message\n'
