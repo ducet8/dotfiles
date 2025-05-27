715123f1a95216e40e9c8c42f57e7eaaa463e79b
Duce Tate <ducet8@outlook.com>
2025-05-27 13:40:39 -0500

    fix sed escape logic

--- etc/bash.d/duce/update_profile_version.sh
+++ etc/bash.d/duce/update_profile_version.sh
@@ -1,9 +1,17 @@
 # vim: ft=sh
-# 2023.08.28 - ducet8@outlook.com
+# 2025.05.27 - ducet8@outlook.com
 
 update_profile_version() {
     local file="${BD_HOME}/dotfiles/bash_profile"
-    local current_line="$(grep "bash_profile_date=\"" "${file}")"
-    local new_line="bash_profile_date=\"$(date +%Y.%m.%d)\""
-    sed -i '' "s/${current_line}/${new_line}/" "${file}"
+    local new_date="$(date +%Y.%m.%d)"
+    
+    # Use awk for more reliable text replacement
+    awk -v new_date="${new_date}" '
+    /^bash_profile_date=/ {
+        sub(/bash_profile_date="[^"]*"/, "bash_profile_date=\"" new_date "\"")
+        print
+        next
+    }
+    { print }
+    ' "${file}" > "${file}.tmp" && mv "${file}.tmp" "${file}"
 }
